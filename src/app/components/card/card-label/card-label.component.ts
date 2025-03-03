4e968cd2b8974fa0244bc108977fc8ac9c6827f0
Artchi3 <arturrsilva.ars@gmail.com>
2025-03-03 16:27:04 -0300

    feat: inclusao componentes extras

---
+++ src/app/components/card/card-label/card-label.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-card-label',
+  imports: [],
+  templateUrl: './card-label.component.html',
+  styleUrl: './card-label.component.scss'
+})
+export class CardLabelComponent {
+ 
+  @Input()
+  gameLabel:string = '' 
+  constructor(){}
+  ngOnInit():void{
+
+  }
+}
--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { CardLabelComponent } from "./card-label/card-label.component";
+import { CardPricingComponent } from "./card-pricing/card-pricing.component";
 
 @Component({
   selector: 'app-card',
-  imports: [],
+  imports: [CardLabelComponent, CardPricingComponent],
   templateUrl: './card.component.html',
   styleUrl: './card.component.scss'
 })
-export class CardComponent {
+export class CardComponent{
 
+  @Input()
+  gameCover:string =''
+  @Input()
+  gameLabel:string =''
+  @Input()
+  gamePrice:string =''
+  @Input()
+  gameType:string =''
+  constructor(){}
+  ngOnInit():void{
+
+  }
 }
